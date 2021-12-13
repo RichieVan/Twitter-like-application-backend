2d641f4e1ad196b8bf71e7582013539c41e5115b
RichieVan <richievan2@mail.ru>
2021-12-13 19:47:54 +0500

    mail fix

--- services/MailService.js
+++ services/MailService.js
@@ -1,6 +1,9 @@
 import nodemailer from 'nodemailer';
-import dotenv from 'dotenv';
-dotenv.config();
+
+console.log(process.env.SMTP_HOST);
+console.log(process.env.SMTP_PORT);
+console.log(process.env.SMTP_USER);
+console.log(process.env.SMTP_PASS);
 
 class MailService {
     constructor () {
